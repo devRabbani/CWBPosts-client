0d583547c2f866a080232423151a41e4a859af74
StrikerOne65 <golamrabbani20014@gmail.com>
2021-08-26 20:46:29 +0530

    slicing main bundle js into chunks of about and contact section using lazy

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import React from 'react'
-import Footer from './Footer'
+import React, { lazy, Suspense } from 'react'
+
 import ToTop from './toTop'
-import Directory from './directory'
+const Footer = lazy(() => import('./Footer'))
+const Directory = lazy(() => import('./directory'))
 
 const App = () => {
   return (
     <div>
-      <ToTop />
-      <div className='mainDiv'>
-        <div className='backgroundCard'>
-          <div className='container home'>
-            <h1 className='h1'>Welcome To CWBPosts</h1>
-            <p>
-              Here you will find some great articles about tech, study,
-              lifestyle. Feel free to contribute and read here. For more info,
-              connect to CanWeBe.
-            </p>
+      <Suspense fallback={null}>
+        <ToTop />
+        <div className='mainDiv'>
+          <div className='backgroundCard'>
+            <div className='container home'>
+              <h1 className='h1'>Welcome To CWBPosts</h1>
+              <p>
+                Here you will find some great articles about tech, study,
+                lifestyle. Feel free to contribute and read here. For more info,
+                connect to CanWeBe.
+              </p>
+            </div>
+          </div>
+          <div className='container'>
+            <h3>Recent Posts</h3>
+            <hr className='mainH1Hr' />
+            <Directory />
           </div>
         </div>
-        <div className='container'>
-          <h3>Recent Posts</h3>
-          <hr className='mainH1Hr' />
-          <Directory />
-        </div>
-      </div>
 
-      <Footer />
+        <Footer />
+      </Suspense>
     </div>
   )
 }
